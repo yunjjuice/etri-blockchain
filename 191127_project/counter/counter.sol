513584214cc8e965b93cf5e17717ae342946a516
최윤주 <yeomju311@gmail.com>
2019-11-27 13:34:33 +0900

    counter에 event 등록하기. 값이 변하면 get버튼을 누르지 않아도 자동으로 화면이 바뀐다

--- 191127_project/counter/counter.sol
+++ 191127_project/counter/counter.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.18;
 
 contract Counter {
-    uint256 count;  // persistent contract storage
+    uint count;
+
+    event Instructor(
+       	uint count
+    );
 
     function Counter(uint256 _count) public {
         count = _count;
@@ -9,9 +13,24 @@ contract Counter {
 
     function increment() public {
         count += 1;
+        emit Instructor(count);  
+    }
+    
+    function decrement() public {
+        count -= 1;
+        emit Instructor(count); 
     }
 
     function getCount() public view returns (uint256) {
         return count;
     }
+    
+    function setInstructor(uint _count) public {
+       count = _count;
+       emit Instructor(_count);        // Add this
+   }
+   
+   function getInstructor() view public returns (uint) {
+       return (count);
+   }
 }
